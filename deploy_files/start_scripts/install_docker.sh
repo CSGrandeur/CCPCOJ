0d6b2cf21eac204360fe9b42eac415646ebf638c
csgrandeur <csgrandeur@gmail.com>
2024-05-04 21:20:04 +0800

    update

--- deploy_files/start_scripts/install_docker.sh
+++ deploy_files/start_scripts/install_docker.sh
@@ -8,7 +8,7 @@ else
     echo "Start to install docker-ce"
     for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
     sudo apt-get update
-    sudo apt-get install ca-certificates curl gnupg
+    sudo apt-get install -y ca-certificates curl gnupg
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -17,7 +17,7 @@ else
       "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo groupadd docker
     sudo gpasswd -a $USER docker
     newgrp docker
