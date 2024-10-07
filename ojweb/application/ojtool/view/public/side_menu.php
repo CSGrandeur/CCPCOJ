66182e8aaea058ecc7df05029b3eac227363dd2d
csgrandeur <csgrandeur@gmail.com>
2024-10-07 17:15:18 +0800

    .

--- ojweb/application/ojtool/view/public/side_menu.php
+++ ojweb/application/ojtool/view/public/side_menu.php
@@ -1,7 +1,8 @@
 <hr id="hidden_user_panel">
 <li class="nav-item"><a class="nav-link {if ($controller == 'seatdraw')}active{/if}"     href="__OJTOOL__/seatdraw">机位抽签</a></li>
 <li class="nav-item"><a class="nav-link {if ($controller == 'rankroll')}active{/if}"     href="__OJTOOL__/rankroll">滚榜</a></li>
-<li class="nav-item"><a class="nav-link {if ($controller == 'contestlive')}active{/if}"     href="__OJTOOL__/contestlive">直播</a></li>
-<li class="nav-item"><a class="nav-link {if ($controller == 'exam')}active{/if}"     href="__OJTOOL__/exam">监考辅助</a></li>
+<li class="nav-item"><a class="nav-link {if ($controller == 'contestlive')}active{/if}" href="__OJTOOL__/contestlive">直播</a></li>
+<li class="nav-item"><a class="nav-link {if ($controller == 'tool')}active{/if}"        href="__OJTOOL__/tool/polygon_parser">Polygon转换</a></li>
+<!-- <li class="nav-item"><a class="nav-link {if ($controller == 'exam')}active{/if}"     href="__OJTOOL__/exam">监考辅助</a></li> -->
 <hr>
 <li class="nav-item"><a class="nav-link" href="/">回到OJ</a></li>
